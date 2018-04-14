6a8a3e7095fa26b39901e9b0c4be10eb1e355882
cojna <cojna@users.noreply.github.com>
2018-04-15 00:19:32 +0900

    AtCoder Regular Contest 095 (Haskell)

---
+++ atcoder/arc095/d.hs
@@ -0,0 +1,45 @@
+{-# OPTIONS_GHC -O2 -funbox-strict-fields #-}
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
+
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Control.Monad.State.Strict
+import           Data.Bool
+import qualified Data.ByteString.Char8       as B
+import qualified Data.ByteString.Unsafe      as B
+import           Data.Char
+import           Data.Function
+import qualified Data.IntMap.Strict          as IM
+import qualified Data.IntSet                 as IS
+import qualified Data.List                   as L
+import qualified Data.Map.Strict             as M
+import           Data.Monoid
+import           Data.Ord
+import           Data.Primitive.MutVar
+import qualified Data.Set                    as S
+import           Data.Tuple
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import           Debug.Trace
+import           Foreign                     hiding (void)
+import           Unsafe.Coerce
+
+main :: IO ()
+main = do
+    !n <- readLn :: IO Int
+    xs <- L.unfoldr (B.readInt.B.dropWhile isSpace) <$> B.getLine
+    putStr.unwords.map show$solve n xs
+
+solve :: Int -> [Int] -> [Int]
+solve n xs = [m, res]
+  where
+    !(m:ys) = L.sortBy (flip compare) xs
+    res = L.minimumBy (comparing $ \x -> abs $ 2 * x - m) ys
