75f24c2db13c126e835d1f80f9b29919758e7487
cojna <cojna@users.noreply.github.com>
2018-11-25 17:50:34 +0900

    Improve performance

--- atcoder/dwacon/5th/qual/c.hs
+++ atcoder/dwacon/5th/qual/c.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS_GHC -O2 #-}
-{-# LANGUAGE BangPatterns          #-}
 {-# LANGUAGE CPP                   #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
@@ -44,20 +43,11 @@ main = do
     mapM_ (print.solve cs) ks
 
 solve :: U.Vector Char -> Int -> Int
-solve cs k = fst $ U.foldl' step (U.foldl' step0 (0, emptyQ) pre) $ U.zip cs post
+solve cs k = score $ U.foldl' step (U.foldl' step0 emptyQ pre) $ U.zip cs post
   where
     (pre, post) = U.splitAt k cs
-    step0 (!res, !q) c
-        | c == 'C' = (res + score q', q')
-        | otherwise = (res, q')
-      where
-        q' = push c q
-
-    step (!res, !q) (ck, c)
-        | c == 'C' = (res + score q', q')
-        | otherwise = (res, q')
-      where
-        q' = push c $ pop ck q
+    step0 q c = push c q
+    step q (ck, c) = push c $ pop ck q
 
 data Q = Q !Int !Int !Int !Int
 
@@ -65,15 +55,17 @@ emptyQ :: Q
 emptyQ = Q 0 0 0 0
 
 push :: Char -> Q -> Q
-push 'D' (Q d m dm f) = Q (d + 1) m dm f
-push 'M' (Q d m dm f) = Q d (m + 1) (dm + d) f
-push c q              = q
+push c q@(Q d m dm dmc) = case c of
+    'D' -> Q (d + 1) m dm dmc
+    'M' -> Q d (m + 1) (dm + d) dmc
+    'C' -> Q d m dm (dmc + dm)
+    _   -> q
 
 pop :: Char -> Q -> Q
-pop c (Q d m dm f) = case c of
-    'D' -> Q (d - 1) m (dm - m) (f + 1)
-    'M' -> Q d (m - 1) dm (f + 1)
-    _   -> Q d m dm (f + 1)
+pop c q@(Q d m dm dmc) = case c of
+    'D' -> Q (d - 1) m (dm - m) dmc
+    'M' -> Q d (m - 1) dm dmc
+    _   -> q
 
 score :: Q -> Int
-score (Q _ _ dm _) = dm
+score (Q _ _ _ dmc) = dmc
