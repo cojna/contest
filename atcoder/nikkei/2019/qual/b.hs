99f291cb40d0c0d034b4217090aebeb1927c8241
cojna <cojna@users.noreply.github.com>
2019-01-27 23:47:38 +0900

    全国統一プログラミング王決定戦予選/NIKKEI Programming Contest 2019

---
+++ atcoder/nikkei/2019/qual/b.hs
@@ -0,0 +1,50 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TupleSections         #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Control.Monad.State.Strict
+import           Data.Bool
+import qualified Data.ByteString.Char8       as B
+import qualified Data.ByteString.Unsafe      as B
+import           Data.Char
+import           Data.Function
+import qualified Data.IntMap.Strict          as IM
+import qualified Data.IntSet                 as IS
+import           Data.List
+import qualified Data.Map.Strict             as M
+import           Data.Monoid
+import           Data.Ord
+import           Data.Primitive.MutVar
+import qualified Data.Set                    as S
+import           Data.Tuple
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import           Debug.Trace
+import           Foreign                     hiding (void)
+import           Unsafe.Coerce
+
+main :: IO ()
+main = do
+    n <- readLn :: IO Int
+    [xs, ys, zs] <- replicateM 3 getLine
+    print $ solve xs ys zs
+
+solve xs ys zs = sum $ zipWith3 f xs ys zs
+f x y z
+  | x == y, y == z = 0
+  | x == y = 1
+  | y == z = 1
+  | z == x = 1
+  | otherwise = 2
---
+++ atcoder/nikkei/2019/qual/c.hs
@@ -0,0 +1,62 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TupleSections         #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Control.Monad.State.Strict
+import           Data.Bool
+import qualified Data.ByteString.Char8       as B
+import qualified Data.ByteString.Unsafe      as B
+import           Data.Char
+import           Data.Function
+import qualified Data.IntMap.Strict          as IM
+import qualified Data.IntSet                 as IS
+import           Data.List
+import qualified Data.Map.Strict             as M
+import           Data.Monoid
+import           Data.Ord
+import           Data.Primitive.MutVar
+import qualified Data.Set                    as S
+import           Data.Tuple
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import           Debug.Trace
+import           Foreign                     hiding (void)
+import           Unsafe.Coerce
+
+main :: IO ()
+main = do
+    !n <- readLn :: IO Int
+    xys <- U.unfoldrN n parseInt2 <$> B.getContents
+    print $ solve n xys
+
+solve :: Int -> U.Vector (Int, Int) -> Int
+solve n xys = go 0 . map snd . sortBy (flip compare)
+    . U.toList
+    $ U.map (\(x, y) -> (x + y, (x, y))) xys
+  where
+    go !acc ((x, _):(_, y):xys) = go (acc + x - y) xys
+    go acc [(x, _)] = acc + x
+    go acc [] = acc
+
+-------------------------------------------------------------------------------
+type Parser a = B.ByteString -> Maybe (a, B.ByteString)
+
+parseInt :: Parser Int
+parseInt = B.readInt . B.dropWhile isSpace
+
+parseInt2 :: Parser (Int, Int)
+parseInt2 = runStateT $
+    (,) <$> StateT (B.readInt . B.dropWhile isSpace)
+        <*> StateT (B.readInt . B.unsafeTail)
