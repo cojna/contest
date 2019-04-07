0fcfa47d578030971f504f3a7ad0d6be70255b76
cojna <cojna@users.noreply.github.com>
2019-04-07 23:58:52 +0900

    Cake 123 - AtCoder Beginner Contest 123

--- atcoder/abc123/d.hs
+++ atcoder/abc123/d.hs
@@ -48,7 +48,6 @@ main = do
     zs <- U.unfoldrN z (C.readInt.C.dropWhile isSpace) <$> C.getLine
     putStr.unlines.map show $ solve k x y z xs ys zs
 
-
 type IntIntInt = Int
 
 data Node = Node !Int !IntIntInt deriving (Eq, Ord, Show)
@@ -77,25 +76,95 @@ solve k x y z
         (U.reverse.radixSort64 -> xs)
         (U.reverse.radixSort64 -> ys)
         (U.reverse.radixSort64 -> zs)
-    = take k . go IS.empty $ S.singleton (node $ encode 0 0 0)
+    = take k . go IS.empty $ _HHsingleton (node $ encode 0 0 0)
   where
     priority (decode -> (i, j, k))
         | i < x, j < y, k < z = U.unsafeIndex xs i + U.unsafeIndex ys j + U.unsafeIndex zs k
         | otherwise = minBound
     node ijk = Node (priority ijk) ijk
 
-    go !visited !set = case S.maxView set of
-        Just (Node p ijk, set')
-            | IS.notMember ijk visited -> p : go (IS.insert ijk visited) set''
-            | otherwise -> go visited set''
+    go !visited !set = case _HHdeleteFindMax set of
+        Just (Node p ijk, set') -> p : go visited' set''
           where
-            set'' = F.foldl' (flip S.insert) set'
-                . map node
-                . filter (flip IS.notMember visited)
+            nexts = filter (flip IS.notMember visited)
                 $ map ($ ijk) [succX, succY, succZ]
+            visited' = F.foldl' (flip IS.insert) visited nexts
+            set'' = F.foldl' (flip _HHinsert) set' $ map node nexts
         Nothing -> []
 
 -------------------------------------------------------------------------------
+
+data MaxHeap a = MaxFork !a [MaxHeap a] | MaxEmpty
+
+_HHempty :: MaxHeap a
+_HHempty = MaxEmpty
+{-# INLINE _HHempty #-}
+
+_HHsingleton :: a -> MaxHeap a
+_HHsingleton x = MaxFork x []
+{-# INLINE _HHsingleton #-}
+
+_HHnull :: MaxHeap a -> Bool
+_HHnull MaxEmpty      = True
+_HHnull (MaxFork _ _) = False
+{-# INLINE _HHnull #-}
+
+_HHinsert :: Ord a => a -> MaxHeap a -> MaxHeap a
+_HHinsert x = _HHmerge (MaxFork x [])
+{-# INLINE _HHinsert #-}
+
+_HHMaxElem :: MaxHeap a -> Maybe a
+_HHMaxElem (MaxFork x _) = Just x
+_HHMaxElem MaxEmpty      = Nothing
+{-# INLINE _HHMaxElem #-}
+
+_HHdeleteMax :: Ord a => MaxHeap a -> Maybe (MaxHeap a)
+_HHdeleteMax (MaxFork _ hs) = Just $ _HHmergePairs hs
+_HHdeleteMax MaxEmpty       = Nothing
+{-# INLINE _HHdeleteMax #-}
+
+_HHdeleteFindMax :: Ord a => MaxHeap a -> Maybe (a, MaxHeap a)
+_HHdeleteFindMax (MaxFork x hs) = Just (x, _HHmergePairs hs)
+_HHdeleteFindMax MaxEmpty       = Nothing
+{-# INLINE _HHdeleteFindMax #-}
+
+_HHmerge :: Ord a => MaxHeap a -> MaxHeap a -> MaxHeap a
+_HHmerge hx@(MaxFork x hxs) hy@(MaxFork y hys)
+  | y <= x    = MaxFork x (hy:hxs)
+  | otherwise = MaxFork y (hx:hys)
+_HHmerge MaxEmpty hy = hy
+_HHmerge hx _ = hx
+{-# INLINE _HHmerge #-}
+
+_HHmergePairs :: Ord a => [MaxHeap a] -> MaxHeap a
+_HHmergePairs (x:y:hs) = (x <> y) <> _HHmergePairs hs
+_HHmergePairs [x]      = x
+_HHmergePairs []       = MaxEmpty
+{-# INLINE _HHmergePairs #-}
+
+instance Ord a => Eq (MaxHeap a) where
+    (==) = (==) `on` toList
+
+instance Ord a => Ord (MaxHeap a) where
+    compare = compare `on` toList
+
+instance Ord a => IsList (MaxHeap a) where
+    type Item (MaxHeap a) = a
+    fromList xs = _HHmergePairs $ map _HHsingleton xs
+    toList = L.unfoldr _HHdeleteFindMax
+
+instance (Show a, Ord a) => Show (MaxHeap a) where
+    show = show . toList
+
+instance Ord a => Monoid (MaxHeap a) where
+    mempty = _HHempty
+    {-# INLINE mempty #-}
+    mconcat = _HHmergePairs
+    {-# INLINE mconcat #-}
+    mappend = _HHmerge
+    {-# INLINE mappend #-}
+
+
 radixSort64 :: U.Vector Int -> U.Vector Int
 radixSort64 v = F.foldl' step v [0, 16, 32, 48]
   where
