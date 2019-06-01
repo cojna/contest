38ea0b2dbb96993cb5a5361a3e090822fc2d3835
cojna <cojna@users.noreply.github.com>
2019-06-02 01:02:49 +0900

    M-SOLUTIONS プロコンオープン

---
+++ atcoder/m-solutions/e.hs
@@ -0,0 +1,163 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MagicHash             #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf            #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TupleSections         #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Control.Monad.State.Strict
+import           Data.Bool
+import qualified Data.ByteString             as B
+import qualified Data.ByteString.Builder     as B
+import qualified Data.ByteString.Char8       as C
+import qualified Data.ByteString.Unsafe      as B
+import           Data.Char
+import qualified Data.Foldable               as F
+import           Data.Function
+import qualified Data.IntMap.Strict          as IM
+import qualified Data.IntSet                 as IS
+import qualified Data.List                   as L
+import qualified Data.Map.Strict             as M
+import           Data.Monoid
+import           Data.Ord
+import           Data.Primitive.MutVar
+import qualified Data.Set                    as S
+import           Data.Tuple
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import           Debug.Trace
+import           Foreign                     hiding (void)
+import           GHC.Exts
+import qualified System.IO                   as IO
+import           Unsafe.Coerce
+
+main :: IO ()
+main = do
+    !n <- readLn :: IO Int
+    qs <- U.unfoldrN n parseInt3 <$> C.getContents
+    B.hPutBuilder IO.stdout.U.foldr (\x acc -> B.intDec x <> B.char7 '\n' <> acc) mempty $ solve n qs
+
+solve :: Int -> U.Vector (Int, Int, Int) -> U.Vector Int
+solve _ = U.map solve'
+
+solve' :: (Int, Int, Int) -> Int
+solve' (x, d, n)
+    | d == 0 = x ^% n
+    | n >= modulus = 0
+    | xd + n - 1 >= modulus = 0
+    | otherwise = (d ^% n) *% (fact (xd + n - 1) /% fact (xd - 1))
+  where
+    !xd = x /% d
+
+-------------------------------------------------------------------------------
+#define MOD 1000003
+
+modulus :: Int
+modulus = MOD
+
+infixr 8 ^%
+infixl 7 *%, /%
+infixl 6 +%, -%
+
+type IntMod = Int
+
+intMod :: Int -> IntMod
+intMod x = rem x MOD
+
+intModValidate :: Int -> Bool
+intModValidate x = 0 <= x && x < MOD
+
+(+%) :: IntMod -> IntMod -> IntMod
+(I# x#) +% (I# y#) = case x# +# y# of
+    r# -> I# (r# -# ((r# >=# MOD#) *# MOD#))
+{-# INLINE (+%) #-}
+
+(-%) :: IntMod -> IntMod -> IntMod
+(I# x#) -% (I# y#) = case x# -# y# of
+    r# -> I# (r# +# ((r# <# 0#) *# MOD#))
+{-# INLINE (-%) #-}
+
+(*%) :: IntMod -> IntMod -> IntMod
+(I# x#) *% (I# y#) = I# ((x# *# y#) `remInt#` MOD#)
+{-# INLINE (*%) #-}
+
+(/%) :: IntMod -> IntMod -> IntMod
+(I# x#) /% (I# y#) = go# y# MOD# 1# 0#
+  where
+    go# a# b# u# v#
+        | isTrue# (b# ># 0#) = case a# `quotInt#` b# of
+            q# -> go# b# (a# -# (q# *# b#)) v# (u# -# (q# *# v#))
+        | otherwise = I# ((x# *# (u# +# MOD#)) `remInt#` MOD#)
+{-# INLINE (/%) #-}
+
+(^%) :: IntMod -> Int -> IntMod
+x ^% n
+    | n > 0 = go 1 x n
+    | n == 0 = 1
+    | otherwise = go 1 (1 /% x) (-n)
+  where
+    go !acc !y !m
+        | m .&. 1 == 0 = go acc (y *% y) (unsafeShiftR m 1)
+        | m == 1 = acc *% y
+        | otherwise = go (acc *% y) (y *% y) (unsafeShiftR (m - 1) 1)
+
+fact :: Int -> IntMod
+fact = U.unsafeIndex factCache
+{-# INLINE fact #-}
+
+recipFact :: Int -> IntMod
+recipFact = U.unsafeIndex recipFactCache
+{-# INLINE recipFact #-}
+
+comb :: Int -> Int -> IntMod
+comb n k = fact n *% recipFact (n - k) *% recipFact k
+{-# INLINE comb #-}
+
+factCacheSize :: Int
+factCacheSize = min (modulus - 1) 2000200
+{-# INLINE factCacheSize #-}
+
+factCache :: U.Vector IntMod
+factCache = U.scanl' (*%) 1 $ U.generate factCacheSize (+1)
+{-# NOINLINE factCache #-}
+
+recipFactCache :: U.Vector IntMod
+recipFactCache = U.scanr' (*%) (1 /% (factCache U.! factCacheSize))
+    $ U.generate factCacheSize (+1)
+{-# NOINLINE recipFactCache #-}
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Int
+parseInt = C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Int, Int)
+parseInt2 = runStateT $
+    (,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+
+parseInt3 :: Parser (Int, Int, Int)
+parseInt3 = runStateT $
+    (,,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
+
+parseInt4 :: Parser (Int, Int, Int, Int)
+parseInt4 = runStateT $
+    (,,,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
