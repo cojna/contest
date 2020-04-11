8507e5e214bccaae709e6efba5b24c39ceaf1096
cojna <cojna@users.noreply.github.com>
2020-04-11 17:41:09 +0900

    Use ImplicitParam

--- gcj/2020/qual/d.hs
+++ gcj/2020/qual/d.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, CPP #-}
+{-# LANGUAGE BangPatterns, CPP, ImplicitParams, RankNTypes, ConstraintKinds#-}
 
 import           Control.Monad
 import           Control.Monad.Primitive
@@ -6,23 +6,25 @@ import           Data.Function
 import qualified Data.List                   as L
 import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as UM
+import           System.Environment
 import qualified System.IO                   as IO
+import           System.IO
 import           System.Process
 
 
 main :: IO ()
-main = runInteractive $ \hin hout b -> do
+main = withInteractiveJudge $ \b -> do
     buf <- UM.replicate b (-1)
     let other i = b - 1 - i
 
     flip fix (0, Nothing, Nothing) $ \loop (!pos, !comp, !rev) -> do
         case (comp, rev) of
             (Just (posC, prevC), Just (posR, prevR)) -> do
-                [curC, curR] <- query hin hout [posC, posR]
+                [curC, curR] <- query [posC, posR]
                 let queryL = map (`mod` b) [pos..pos+3]
                 let queryR = map other queryL
-                ls <- query hin hout queryL
-                rs <- query hin hout queryR
+                ls <- query queryL
+                rs <- query queryR
                 when (prevC /= curC) $ do
                     flipBuf buf
                 when (prevR /= curR && prevC == curC || prevR == curR && prevC /= curC) $ do
@@ -33,11 +35,11 @@ main = runInteractive $ \hin hout b -> do
                 when (pos + 3 < div b 2) $ do
                     loop (pos + 4, Just (posC, curC), Just (posR, curR))
             (Just (posC, prevC), Nothing) -> do
-                [curC, _] <- query hin hout [posC, posC]
+                [curC, _] <- query [posC, posC]
                 let queryL = map (`mod` b) [pos..pos+3]
                 let queryR = map other queryL
-                ls <- query hin hout queryL
-                rs <- query hin hout queryR
+                ls <- query queryL
+                rs <- query  queryR
                 when (prevC /= curC) $ do
                     flipBuf buf
                 writeBuf buf queryL ls
@@ -47,11 +49,11 @@ main = runInteractive $ \hin hout b -> do
                 when (pos + 3 < div b 2) $ do
                     loop (pos + 4, Just (posC, curC), rev')
             (Nothing, Just (posR, prevR)) -> do
-                [curR, _] <- query hin hout [posR, posR]
+                [curR, _] <- query [posR, posR]
                 let queryL = map (`mod` b) [pos..pos+3]
                 let queryR = map other queryL
-                ls <- query hin hout queryL
-                rs <- query hin hout queryR
+                ls <- query  queryL
+                rs <- query  queryR
                 when (prevR /= curR) $ do
                     revBuf buf
                 writeBuf buf queryL ls
@@ -63,8 +65,8 @@ main = runInteractive $ \hin hout b -> do
             (Nothing, Nothing) -> do
                 let queryL = [pos..pos+4]
                 let queryR = map other queryL
-                ls <- query hin hout queryL
-                rs <- query hin hout queryR
+                ls <- query  queryL
+                rs <- query  queryR
                 writeBuf buf queryL ls
                 writeBuf buf queryR rs
                 let comp' = fmap (\(i, l, _) -> (i, l))
@@ -72,9 +74,8 @@ main = runInteractive $ \hin hout b -> do
                 let rev' = fmap (\(i, l, _) -> (i, l))
                         . L.find (\(_, l, r) -> l /= r) $ zip3 queryL ls rs
                 loop (pos + 5, comp', rev')
-    hDumpBuf hout buf
-    IO.hFlush hout
-    "Y" <- IO.hGetLine hin
+    hDumpBuf buf
+    "Y" <- recvLine
     return ()
 
 writeBuf :: (PrimMonad m) => UM.MVector (PrimState m) Int -> [Int] -> [Int] -> m ()
@@ -83,10 +84,10 @@ writeBuf mv ks vs = do
         UM.unsafeWrite mv k v
 
 
-hDumpBuf :: IO.Handle -> UM.MVector (PrimState IO) Int -> IO ()
-hDumpBuf h mv = do
+hDumpBuf :: HasInteractiveJudge => UM.MVector (PrimState IO) Int -> IO ()
+hDumpBuf mv = do
     v <- U.freeze mv
-    IO.hPutStrLn h . concatMap show $ U.toList v
+    sendStrLn . concatMap show $ U.toList v
 
 
 revBuf :: (PrimMonad m) => UM.MVector (PrimState m) Int -> m ()
@@ -101,11 +102,10 @@ flipBuf mv = do
         UM.unsafeModify mv (1-) i
 
 
-query :: IO.Handle -> IO.Handle -> [Int] -> IO [Int]
-query hin hout poss = do
-    mapM_ (IO.hPrint hout . succ) poss
-    IO.hFlush hout
-    res <- mapM (const $ read <$> IO.hGetLine hin) poss
+query :: HasInteractiveJudge => [Int] -> IO [Int]
+query poss = do
+    mapM_ (send . succ) poss
+    res <- mapM (const $ recvLn) poss
     return res
 
 
@@ -113,18 +113,48 @@ rep :: Monad m => Int -> (Int -> m ()) -> m ()
 rep !n = U.forM_ $ U.generate n id
 {-# INLINE rep #-}
 
-
-runInteractive :: (IO.Handle -> IO.Handle -> Int -> IO ()) -> IO ()
-runInteractive action = do
-#ifdef DEBUG
-    (Just hout, Just hin, _, _) <- createProcess
-        (shell "python3 local_testing_tool.py 2")
-            { std_in = CreatePipe, std_out = CreatePipe}
-#else
-    let hin = IO.stdin
-    let hout = IO.stdout
-#endif
-    [t, b] <- map read.words <$> IO.hGetLine hin :: IO [Int]
+withInteractiveJudge :: (HasInteractiveJudge => Int -> IO ()) -> IO ()
+withInteractiveJudge action = do
+    judge <- newInteractiveJudge
+    let ?interactiveJudge = judge
+    [t, b] <- map read.words <$> recvLine :: IO [Int]
     forM_ [1..t] $ \i -> do
-        IO.hPutStrLn IO.stderr $ "Case #" ++ shows i ":"
-        action hin hout b
+        hPutStrLn stderr $ "Case #" ++ shows i ":"
+        action b
+
+data InteractiveJudge = InteractiveJudge
+    { hin :: Handle
+    , hout :: Handle
+    }
+
+type HasInteractiveJudge = (?interactiveJudge :: InteractiveJudge)
+
+newInteractiveJudge :: IO InteractiveJudge
+newInteractiveJudge = do
+    args <- getArgs
+    case args of
+        [] -> return $ InteractiveJudge stdin stdout
+        _ -> do
+            (Just i, Just o, _, _) <- createProcess
+                (shell . unwords $ "python3 local_testing_tool.py": args)
+                    { std_in = CreatePipe, std_out = CreatePipe}
+            return $ InteractiveJudge o i
+
+send :: (Show a, HasInteractiveJudge) => a -> IO ()
+send = sendStrLn . show
+
+sendStr :: HasInteractiveJudge => String -> IO ()
+sendStr cs = do
+    IO.hPutStr (hout ?interactiveJudge) cs
+    IO.hFlush (hout ?interactiveJudge)
+
+sendStrLn :: HasInteractiveJudge => String -> IO ()
+sendStrLn cs = do
+    IO.hPutStrLn (hout ?interactiveJudge) cs
+    IO.hFlush (hout ?interactiveJudge)
+
+recvLine :: HasInteractiveJudge => IO String
+recvLine = IO.hGetLine (hin ?interactiveJudge)
+
+recvLn :: (Read a, HasInteractiveJudge) => IO a
+recvLn = read <$> recvLine
