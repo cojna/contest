25fe98487130f13d60d4a22d500d547d2d5715b6
cojna <cojna@users.noreply.github.com>
2020-05-24 23:24:34 +0900

    AtCoder Grand Contest 044

---
+++ atcoder/agc044/a.hs
@@ -0,0 +1,116 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns, BinaryLiterals, CPP, DerivingStrategies  #-}
+{-# LANGUAGE DerivingVia, FlexibleContexts, FlexibleInstances       #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, KindSignatures, LambdaCase #-}
+{-# LANGUAGE MagicHash, MultiParamTypeClasses, MultiWayIf           #-}
+{-# LANGUAGE NumericUnderscores, OverloadedStrings, PatternSynonyms #-}
+{-# LANGUAGE RankNTypes, RecordWildCards, ScopedTypeVariables       #-}
+{-# LANGUAGE StandaloneDeriving, TupleSections, TypeApplications    #-}
+{-# LANGUAGE TypeFamilies, TypeInType, UnboxedTuples, ViewPatterns  #-}
+
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.Reader
+import           Control.Monad.ST
+import           Control.Monad.State.Strict
+import           Data.Bifunctor
+import           Data.Bool
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Builder       as B
+import qualified Data.ByteString.Char8         as C
+import qualified Data.ByteString.Internal      as B
+import qualified Data.ByteString.Unsafe        as B
+import           Data.Char
+import qualified Data.Foldable                 as F
+import           Data.Function
+import           Data.Functor.Identity
+import qualified Data.IntMap.Strict            as IM
+import qualified Data.IntSet                   as IS
+import qualified Data.List                     as L
+import qualified Data.Map.Strict               as M
+import           Data.Monoid
+import           Data.Ord
+import           Data.Primitive
+import           Data.Ratio
+import           Data.Semigroup
+import qualified Data.Set                      as S
+import           Data.Tuple
+import qualified Data.Vector                   as V
+import qualified Data.Vector.Generic           as G
+import qualified Data.Vector.Generic.Mutable   as GM
+import qualified Data.Vector.Mutable           as VM
+import qualified Data.Vector.Primitive         as P
+import qualified Data.Vector.Primitive.Mutable as PM
+import qualified Data.Vector.Unboxed           as U
+import qualified Data.Vector.Unboxed.Mutable   as UM
+import           Debug.Trace
+import           Foreign                       hiding (void)
+import           GHC.Exts
+import qualified System.IO                     as IO
+import           Unsafe.Coerce
+
+main :: IO ()
+main = do
+    t <- readLn :: IO Int
+    replicateM_ t $ do
+        [n, a, b, c, d] <- map read.words <$> getLine :: IO [Integer]
+        print $ solve n a b c d
+
+memoFixIntMap
+    :: ((Int -> State (IM.IntMap a) a) -> Int -> State (IM.IntMap a) a)
+    -> Int -> a
+memoFixIntMap f n = flip evalState IM.empty $ do
+    flip fix n $ \memo x -> do
+        gets (IM.lookup x) >>= \case
+            Just fx -> pure fx
+            Nothing -> f memo x >>= \fx ->
+                modify' (IM.insert x fx) *> pure fx
+
+solve :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+solve n a b c d = memoFixIntMap f (fromIntegral n)
+  where
+    step memo w k x = case quotRem x k of
+        (q, 0) -> (+w) <$!> memo q
+        (q, r) -> min
+            <$> ((+(w + d * fromIntegral r)) <$!> memo (fromIntegral q))
+            <*> ((+(w + d * fromIntegral (k - r))) <$!> memo (fromIntegral q + 1))
+    f _ 0 = pure 0
+    f _ 1 = pure d
+    f memo x = F.foldl' min (fromIntegral x * d) <$!> sequence
+        [ step memo a 2 x
+        , step memo b 3 x
+        , step memo c 5 x
+        ]
+-------------------------------------------------------------------------------
+-- Utils
+-------------------------------------------------------------------------------
+rep :: Monad m => Int -> (Int -> m ()) -> m ()
+rep !n = U.forM_ $ U.generate n id
+{-# INLINE rep #-}
+rev :: Monad m => Int -> (Int -> m ()) -> m ()
+rev !n = U.forM_ $ U.iterateN n (subtract 1) (n - 1)
+{-# INLINE rev #-}
+infixl 8 `shiftRL`, `unsafeShiftRL`
+shiftRL = unsafeShiftRL
+{-# INLINE shiftRL #-}
+unsafeShiftRL (I# x#) (I# i#) = I# (uncheckedIShiftRL# x# i#)
+{-# INLINE unsafeShiftRL #-}
+type Parser a = StateT C.ByteString Maybe a
+runParser :: Parser a -> C.ByteString -> Maybe (a, C.ByteString)
+runParser = runStateT
+{-# INLINE runParser #-}
+int :: Parser Int
+int = coerce $ C.readInt . C.dropWhile isSpace
+{-# INLINE int #-}
+int1 :: Parser Int
+int1 = fmap (subtract 1) int
+{-# INLINE int1 #-}
+char :: Parser Char
+char = coerce C.uncons
+{-# INLINE char #-}
+byte :: Parser Word8
+byte = coerce B.uncons
+{-# INLINE byte #-}
+
