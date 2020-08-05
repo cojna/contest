f2fd335220bc1d1acecc5fcf8d72793aa631cf43
cojna <cojna@users.noreply.github.com>
2020-08-06 02:43:13 +0900

    AtCoder Beginner Contest 074

---
+++ atcoder/abc074/b.hs
@@ -0,0 +1,1 @@
+main=interact$show.f.map read.words;f(n:k:l)=sum[2*min(k-x)x|x<-l]
---
+++ atcoder/abc074/c.hs
@@ -0,0 +1,1 @@
+main=interact$unwords.map show.tail.f.map read.words;f[a,b,c,d,e,f]=maximum[[(9^9)*s`div`(s+100*w),s+100*w,s]|x<-[0,a..30],w<-[x,x+b..30],w>0,let l=min(e*w)(f-100*w),y<-[0,c..l],let s=div(l-y)d*d+y]
